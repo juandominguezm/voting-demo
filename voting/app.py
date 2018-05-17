138c2772c27301a2a3e47e81a5b9a10d09aa23c8
Juan Dominguez <superseguro@gmail.com>
2018-05-17 02:21:46 +0000

    changing voting option 3

--- voting/app.py
+++ voting/app.py
@@ -9,8 +9,8 @@ import logging
 import signal
 import sys
 
-optionA = "juan"
-optionB = "francisco"
+optionA = "PING"
+optionB = "PONG"
 name = optionA + " VS " + optionB
 
 redis = Redis(host="redis", db=0)
